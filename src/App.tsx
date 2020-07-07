85e89212d2b9be816cde64685535f0988b7479bf
Karol Szuster <kszuster@protonmail.com>
2020-07-07 21:53:44 +0200

    Import stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { MapTileProps } from './components/map/MapTile'
-import Map, { TileRowProps } from './components/map/Map'
+import { MapTileProps } from './components/map/MapTile';
+import Map, { TileRowProps } from './components/map/Map';
+import './App.css';
 
 const rows: TileRowProps[] =
   [1, 2, 3, 4, 5, 6].map(_ => ({"tiles": [1, 2, 3, 4, 5].map(_ => ({"color": "black"} as MapTileProps))} as TileRowProps));
